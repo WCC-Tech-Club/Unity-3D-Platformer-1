f4bbc710143d7295dc0ada22dded318ad5d6e54c
Walter Geisler <soren.endon@gmail.com>
2015-09-29 17:48:21 -0500

    Change game files folder name, add basic needs to `PlayerController` script

---
+++ Assets/3D Platformer 1/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public sealed class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
--- Assets/3d Platformer/Scripts/PlayerController.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-
-public sealed class PlayerController : MonoBehaviour {
-	
-}
