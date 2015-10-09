c2d6b007741bc872d44979f5ae057ef5c43110ad
Walter Geisler <soren.endon@gmail.com>
2015-10-09 16:20:02 -0500

    Improved LevelManager by implementing a reorderable list from the (free) rotorz reorder able list editor package. Also warns in the inspector when listed levels are not in the build settings.

---
+++ Assets/3D Platformer 1/Scripts/Editor/PropertyDrawers/ReorderableStringArrayPropertyDrawer.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using Rotorz.ReorderableList;
+
+[CustomPropertyDrawer(typeof(ReorderableStringArray), false)]
+public sealed class ReorderableStringArrayPropertyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        SerializedProperty stringsProperty = property.FindPropertyRelative("strings");
+        bool allValid = true;
+
+        EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+        for (int i = 0; i < stringsProperty.arraySize; i++)
+        {
+            bool found = false;
+            bool empty = false;
+
+            for (int j = 0; j < buildScenes.Length; j++)
+            {
+                string listValue = stringsProperty.GetArrayElementAtIndex(i).stringValue;
+                if (listValue.Length == 0)
+                {
+                    empty = true;
+                    break;
+                }
+                if (buildScenes[j].path.Contains(stringsProperty.GetArrayElementAtIndex(i).stringValue + ".unity"))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found || empty)
+            {
+                allValid = false;
+                break;
+            }
+        }
+
+        return EditorGUIUtility.singleLineHeight * 1.5f +
+            (!allValid ? EditorGUIUtility.singleLineHeight * 2 : 0) +
+            ReorderableListGUI.CalculateListFieldHeight(stringsProperty);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        SerializedProperty stringsProperty = property.FindPropertyRelative("strings");
+        bool allValid = true;
+
+        EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+        for (int i = 0; i < stringsProperty.arraySize; i++)
+        {
+            bool found = false;
+            bool empty = false;
+
+            for (int j = 0; j < buildScenes.Length; j++)
+            {
+                string listValue = stringsProperty.GetArrayElementAtIndex(i).stringValue;
+                if (listValue.Length == 0)
+                {
+                    empty = true;
+                    break;
+                }
+                if (buildScenes[j].path.Contains(stringsProperty.GetArrayElementAtIndex(i).stringValue + ".unity"))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found || empty)
+            {
+                allValid = false;
+                break;
+            }
+        }
+
+        ReorderableListStyles.Title.fontStyle = FontStyle.Bold;
+
+        Rect titlePosition = new Rect(position);
+        titlePosition.height = EditorGUIUtility.singleLineHeight * 1.5f;
+        ReorderableListGUI.Title(titlePosition, label);
+
+        Rect listPosition = new Rect(position);
+
+        if (!allValid)
+        {
+            Rect helpBoxPosition = new Rect(position);
+            helpBoxPosition.height = EditorGUIUtility.singleLineHeight * 2;
+            helpBoxPosition.y += titlePosition.height;
+
+            listPosition.height = position.height - (titlePosition.height + helpBoxPosition.height);
+            listPosition.y += (titlePosition.height + helpBoxPosition.height);
+            
+            EditorGUI.HelpBox(helpBoxPosition, "Not all levels are found in build settings!", MessageType.Warning);
+        }
+        else
+        {
+            listPosition.height = position.height - titlePosition.height;
+            listPosition.y += titlePosition.height;
+        }
+
+        ReorderableListGUI.ListFieldAbsolute(listPosition, property.FindPropertyRelative("strings"));
+    }
+}
---
+++ Assets/3D Platformer 1/Scripts/Utility/ReorderableStringArray.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using System;
+
+[Serializable]
+public sealed class ReorderableStringArray
+{
+    [SerializeField]
+    private string[] strings;
+
+    public int Length { get { return strings.Length; } }
+
+    public string this[int index] { get { return strings[index]; } }
+}
