8dca7095d6b1e0f359083f0fe8a936cd706c318f
Walter Geisler <soren.endon@gmail.com>
2015-10-14 12:29:35 -0500

    Made end of level region visible in the editor.

--- Assets/3D Platformer 1/Scripts/Level/EndOfLevel.cs
+++ Assets/3D Platformer 1/Scripts/Level/EndOfLevel.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public sealed class EndOfLevel : MonoBehaviour
 {
     [SerializeField]
@@ -11,5 +12,20 @@ public sealed class EndOfLevel : MonoBehaviour
         {
             menuSwitcher.SwitchToFinal();
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.25f);
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+
+        Vector3 center = boxCollider.center;
+        center.Scale(transform.lossyScale);
+        center += transform.position;
+
+        Vector3 size = boxCollider.size;
+        size.Scale(transform.lossyScale);
+
+        Gizmos.DrawCube(center, size);
     }
 }
