b87cb8c59eb19718f8d5f3f9083a51a1c2e6d8ed
Walter Geisler <soren.endon@gmail.com>
2015-10-16 21:00:31 -0500

    Prototype piston

---
+++ Assets/3D Platformer 1/Scripts/Prototype/Piston.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Piston : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody pistonHead;
+    [SerializeField]
+    private float force;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            pistonHead.AddRelativeForce(Vector3.up * force, ForceMode.Impulse);
+        }
+    }
+}
