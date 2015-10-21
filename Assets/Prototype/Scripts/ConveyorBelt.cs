7694fc9f1170644a45bf1cfb0046b91147563ab9
Walter Geisler <soren.endon@gmail.com>
2015-10-21 06:30:14 -0500

    Prototype Conveyor

---
+++ Assets/Prototype/Scripts/ConveyorBelt.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public ConveyorSegment prefab;
+    public int size;
+    public float offset;
+    public float speed;
+
+    private new Transform transform;
+
+    private ConveyorSegment[] segments;
+    private float end;
+
+    void Awake()
+    {
+        transform = base.transform;
+        
+        segments = new ConveyorSegment[size];
+        end = offset * size;
+
+        for (int i = 0; i < size; i++)
+        {
+            Vector3 newSegmentPosition = transform.position + (transform.forward * offset * i);
+            ConveyorSegment newSegment = Instantiate(prefab, newSegmentPosition, Quaternion.identity) as ConveyorSegment;
+            newSegment.transform.parent = transform;
+            segments[i] = newSegment;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        foreach (ConveyorSegment segment in segments)
+        {
+            Vector3 position = segment.Rigidbody.position;
+            position += transform.forward * speed * Time.deltaTime;
+
+            float distance = Vector3.Distance(position, transform.position);
+
+            if (distance < end)
+            {
+                segment.Rigidbody.MovePosition(position);
+            }
+            else
+            {
+                position = transform.position;
+                position += transform.forward * (distance - end);
+                segment.Rigidbody.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/Prototype/Scripts/ConveyorSegment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class ConveyorSegment : MonoBehaviour
+{
+    public Rigidbody Rigidbody { get; private set; }
+
+    void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+}
