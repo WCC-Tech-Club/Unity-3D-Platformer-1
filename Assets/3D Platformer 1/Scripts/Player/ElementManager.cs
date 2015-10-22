90f141c1c4d4b2ded28a7f825f2c532f4e2512b3
Walter Geisler <soren.endon@gmail.com>
2015-10-22 11:19:30 -0500

    Begin design of elemental manager for the player

---
+++ Assets/3D Platformer 1/Scripts/Player/ElementManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+using System;
+
+public class ElementManager : MonoBehaviour
+{
+    [SerializeField]
+    private Element element;
+
+    public Element Element
+    {
+        get { return element; }
+        set { element = Enum.IsDefined(typeof(Element), value) ? value : Element.None; }
+    }
+
+    void Update()
+    {
+        switch (element)
+        {
+        case Element.None:
+        default:
+            // Default stuff
+            break;
+        case Element.Fire:
+            // Fire stuff
+            break;
+        case Element.Ice:
+            // Ice stuff
+            break;
+        case Element.Electrical:
+            // Electrical stuff
+            break;
+        }
+    }
+}
+
+public enum Element : byte
+{
+    None,
+    Fire,
+    Ice,
+    Electrical
+}
