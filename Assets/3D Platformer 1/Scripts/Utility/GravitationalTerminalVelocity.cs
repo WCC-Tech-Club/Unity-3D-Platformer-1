267a62dcbb023e16bdc88055b0d608c2591349a6
Walter Geisler <soren.endon@gmail.com>
2015-10-27 23:32:46 -0500

    Implemented terminal velocity

---
+++ Assets/3D Platformer 1/Scripts/Utility/GravitationalTerminalVelocity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravitationalTerminalVelocity : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.useGravity = false;
+        Vector3 velocity = rigidbody.velocity;
+        velocity.y += (Physics.gravity.y - (velocity.y * rigidbody.drag)) * Time.deltaTime;
+        rigidbody.velocity = velocity;
+    }
+}
