6c99ac5f173b8808fb2cbcea6bd189a8a978bbb9
Walter Geisler <soren.endon@gmail.com>
2015-11-17 12:05:11 -0600

    Implement exploding box prototype when player gets near

---
+++ Assets/3D Platformer 1/Scripts/Level/Explosive.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosive : MonoBehaviour
+{
+    [SerializeField]
+    private Transform explosionPrefab;
+    [SerializeField]
+    private float spreadRadius = 1.5f;
+    [SerializeField]
+    private float spreadDelay = 0.2f;
+
+    private bool exploding;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!exploding && collider.gameObject.tag == "Player" && collider.GetComponent<ElementManager>().IsFire)
+        {
+            StartCoroutine(Explode());
+        }
+    }
+
+    private void ExplosionSpread()
+    {
+        if (!exploding)
+        {
+            StartCoroutine(Explode(spreadDelay));
+        }
+    }
+
+    private IEnumerator Explode(float delay = 0)
+    {
+        exploding = true;
+
+        if (delay > 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        foreach (Collider collider in Physics.OverlapSphere(transform.position, spreadRadius))
+        {
+            if (collider.gameObject.tag == "Explosive")
+            {
+                collider.GetComponent<Explosive>().ExplosionSpread();
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
